e31de6920d77b96db9dda88a88a2a3900f4e8436
22mahmoud <mahmouds1997@yahoo.com>
2018-06-30 22:20:45 +0200

    add redux and redux thunk to the project

---
+++ src/actions/match.js
@@ -0,0 +1,6 @@
+import { TEST_1 } from './actionTypes';
+
+export const setName = name => ({
+  type: TEST_1,
+  payload: name,
+});
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+const rootReducers = combineReducers({
+  no1: () => ({}),
+});
+
+export default rootReducers;
